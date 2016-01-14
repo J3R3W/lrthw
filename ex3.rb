40d3be53ceff0d9746e5bd7fb219a12aac6d0061
Mothilde <vivou_kurt@hotmail.fr>
2016-01-14 17:35:53 +0100

    Fini les exercices 1 à 3

---
+++ ex3.rb
@@ -0,0 +1,27 @@
+# les poulets
+puts "I will now count my chickens:"
+#Poules
+puts "Hens #{25.0 + 30.0 / 6.0}"
+#Coqs
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+#les oeufs
+puts "Now I will count the eggs:"
+#calcul
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#question
+puts "Is it true that 3 + 2 < 5 - 7?"
+#réponse
+puts 3 + 2 < 5 - 7
+#question
+puts "What is 3 + 2? #{3 + 2}"
+#question
+puts "What is 5 - 7? #{5 - 7}"
+#question
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+puts "hieth #{5.0 / 2.0}"
